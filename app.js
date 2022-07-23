c109bba616c058f247268b63cc176ae19622ff35
nahuelschbap <nahuelschbap@hotmail.com>
2022-07-23 19:56:44 -0300

    Entrega-Simulador-Interactivo

--- app.js
+++ app.js
@@ -1,35 +1,10 @@
-console.log("hola mundo!")
-const anioActual = 2022
+const valorDolar = 342
 
-let usuario = prompt("Ingrese el nombre de usuario")
-let anioNacimiento = prompt("Ingrese su año de nacimiento")
-let mensajeSalida = "Bienvenido " + usuario + ". Tu edad es :" +(anioActual - anioNacimiento)
+let pesosArgentinos = prompt("Ingrese cuantos pesos Argentinos desea convertir")
+let mensajeSalida = "Bienvenido " + ". Ese valor equivale a :" +(pesosArgentinos / valorDolar) + " dolares ($USD)" ;
 
 
-console.log(usuario)
-console.log(anioNacimiento)
+console.log(pesosArgentinos)
 alert(mensajeSalida)
 
-var encabezado = document.querySelector("h1")
-encabezado.textContent = "Query Selector";
 
-
-// ENTREGA COMPLEMENTARIA - Crear un algoritmo utilizando un ciclo //
-//for
-
-for (let i = 1; i <=4; i++) {
-    console.log(i)
-}
-console.log('Fin del ciclo')
-
-//while
-
-let edad = prompt('Ingrese su edad')
-
-
-while (edad <= 18 ){
-alert("Usted no cumple la edad necesaria para esta operacion")
-edad = prompt('Ingrese su edad')
-}
-
-alert('Bienvenido')
