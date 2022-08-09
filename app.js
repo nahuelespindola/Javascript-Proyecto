edf8756bff2acf3413aa5b82ef898d3fff18ea85
nahuelschbap <nahuelschbap@hotmail.com>
2022-08-09 19:07:22 -0300

    Interactuando-con-HTML

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
-
 // Objetos & Array - Listado de cambios //
-const monedas = [
-    {
+const monedas = [{
         id: 1,
         nombre: "Dolar Oficial",
         valor: 131
-    }, 
+    },
     {
         id: 2,
         nombre: "Dolar Blue",
         valor: 342
-    }, 
+    },
     {
         id: 3,
         nombre: "Peso Argentino",
         valor: 1
-    }, 
+    },
     {
         id: 4,
         nombre: "Real",
         valor: 0.03939
-    }, 
+    },
     {
         id: 5,
         nombre: "Euro",
         valor: 0.00745
-    }, 
+    },
     {
         id: 6,
         nombre: "Libra Esterlina",
         valor: 0.00625
-    }, 
+    },
     {
         id: 7,
         nombre: "Yen Japones",
@@ -40,18 +38,19 @@ const monedas = [
 console.log(monedas)
 
 
-    //DOM + Evento//
+//DOM + Evento//
 
-    const input = document.getElementById("input")
-    const output = document.getElementById('output')
-    input.addEventListener('keypress', () => {
-output.setAttribute('value', input.value / 30)
-    })
+const input = document.getElementById("input")
+const output = document.getElementById('output')
+input.addEventListener('keypress', () => {
+    output.setAttribute('value', input.value / 30)
+})
 
 
-    //SELECTOR
+//SELECTOR
 
-    const dolarblue = document.querySelector('.USDB');
-    USDB.addEventListener('click', () => {
-        output.setAttribute('value', input.value * 30)
-            })
+const dolarblue = document.getElementById('.USDB');
+USDB.addEventListener('click', () => {
+    output.setAttribute('value', input.value * 100)
+})
+console.log('dolarblue')
